d5ae586216d1e37ccfff495c386e6038d9cad2ca
Mohammad Fuhad Uddin <mohammedfuhad1@gmail.com>
2021-11-11 02:48:54 +0400

    Update index.js
    removing student info

--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
-/*********************************************************************************
- *  WEB422 – Assignment 3
- *  I declare that this assignment is my own work in accordance with Seneca Academic Policy.
- *  No part of this assignment has been copied manually or electronically from any other source
- *  (including web sites) or distributed to other students.
- *
- *  Name: Mohammad Fuhad Uddin Student ID: 135341196 Date: 15/10/2021
- *
- *
- ********************************************************************************/
+
 
 import React from "react";
 import ReactDOM from "react-dom";
